0270ac9a0c6a451c2c6920dc000ee95553afa865
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-03-11 04:45:36 -0400

    added first github look up method

--- hub/core.py
+++ hub/core.py
@@ -1,10 +1,31 @@
+#!/usr/bin/env python
 """
-hub 
+hub
 
 this file contains github api stuff
 """
 
 
+import urllib
+
+import simplejson
+from clint.textui import colored, puts, indent
 
 
+ISSUES_ENDPOINT = 'https://api.github.com/repos/%s/%s/issues'
 
+def get_issues(user, repo):
+    url = ISSUES_ENDPOINT % (user, repo)
+    connect = urllib.urlopen(url)
+    json_data = simplejson.load(connect)
+    puts('Issues: ')
+    for i in json_data:
+        issue_info = '    '.join((
+                '{0}'.format(colored.red(i['number'])),
+                '{0}'.format(colored.cyan(i['user']['login'])),
+                '{0}'.format(i['title'])
+                ))
+        puts(issue_info)
+
+if __name__ == '__main__':
+    exit(get_issues('myusuf3','courtside'))
