97eab3c911f953dc2fb4cf2853e5b2a44272aa34
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-04-19 13:04:51 -0400

    clean up

--- octogit/config.py
+++ octogit/config.py
@@ -1,27 +1,42 @@
 import os
 import ConfigParser
 
-def set_password():
-	pass
+
+def commit_changes(config):
+    '''
+    Write changes to the config file. 
+    '''
+    pass
+
+def create_config():
+    # ran the first time login in run
+    if os.path.exists(octogit_config):
+        pass
+    else:
+        os.makedirs(os.path.dirname(octogit_config))
+        open(octogit_config, 'w').close()
+        config = ConfigParser.ConfigParser()
+        config.add_section('octogit')
 
 
-def set_username():
-	pass
+def set_password(config, password):
+    '''
+    Given a config set the password attribute
+    in the Octogit section. 
+    '''
+    pass
+
+def set_username(config, username):
+    '''
+    Given a config set the username attribute
+    in the Octogit section. 
+    '''
+    pass
 
 
 def login():
-	octogit_config = os.path.expanduser('~/.config/octogit/config.ini')
+    octogit_config = os.path.expanduser('~/.config/octogit/config.ini')
 
-	# ran the first time login in run
-	if os.path.exists(octogit_config):
-		pass
-	else:
-		os.makedirs(os.path.dirname(octogit_config))
-		open(octogit_config, 'w').close()
-
-	config = ConfigParser.ConfigParser()
-	config.read(octogit_config)
-	config.add_section('octogit')
-	config.set('octogit', 'int', '15')
-	with open(octogit_config, 'w') as configfile:
-		config.write(configfile)
+    config.set('octogit', 'user', '15')
+    with open(octogit_config, 'w') as configfile:
+        config.write(configfile)
