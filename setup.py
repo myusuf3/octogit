bd72543c1008e0df804aba69281656ac11d62334
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-04-21 02:39:07 -0400

    changing it to get around README.rst error

--- setup.py
+++ setup.py
@@ -46,7 +46,37 @@ setup(
     install_requires=dependencies,
     packages=['octogit', ],
     license='MIT License',
-    long_description=open('README.rst').read(),
+    long_description="""
+========
+Octogit
+========
+
+Do you hate this screen? Do you hate switching screens to see issues? Do you love the terminal? Then you will love this project.
+
+During the development of this plugin Github smartened up and introduced a new way to create repositories. Hopefully people who like to stay in the terminal will enjoy this litle cli app. 
+
+.. image:: https://github.com/myusuf3/octogit/raw/gh-pages/assets/img/readme_image.png
+
+
+Installation
+============
+
+`pip install octogit`
+
+
+How to Octogit
+==============
+
+Go to http://myusuf3.github.com/octogit
+
+
+Contribute
+==========
+If you would like to contribute simply fork this project and add yourself to the AUTHORS.txt along with some changes hopefully and submit a pull request.
+
+
+
+    """,
     entry_points={
         'console_scripts': [
             'octogit = octogit.cli:begin',
