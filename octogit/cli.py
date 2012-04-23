2fc1b6f9407af745203cc6d498a0d504b5129462
TR Jordan <terral.jordan@gmail.com>
2012-04-23 15:14:53 +0000

    Update documentation to indicate the existance of assigned flag.

--- octogit/cli.py
+++ octogit/cli.py
@@ -24,7 +24,7 @@ def get_help():
         puts(colored.green('octogit login'))
         puts(colored.green("octogit create <repo> 'description'"))
         puts(colored.green("octogit create <repo> 'description' <organization>"))
-        puts(colored.green('octogit issues'))
+        puts(colored.green('octogit issues [--assigned]'))
         puts(colored.green('octogit issues <number>'))
         puts(colored.green('octogit issues <number> close'))
         puts('\n')
