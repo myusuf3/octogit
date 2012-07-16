bcad5bf76165b5a008714136e44cda3459b0c4ee
Rich Healey <richard.healey@99designs.com>
2012-07-16 00:03:26 -0700

    Store an auth token, not a plaintext password

--- octogit/config.py
+++ octogit/config.py
@@ -5,6 +5,11 @@ import ConfigParser
 import requests
 from clint.textui import colored, puts
 
+try:
+    import json
+except ImportError:
+    import simplejson as json  # NOQA
+
 CONFIG_FILE = os.path.expanduser('~/.config/octogit/config.ini')
 # ran the first time login in run
 config = ConfigParser.ConfigParser()
@@ -26,13 +31,13 @@ def create_config():
         open(CONFIG_FILE, 'w').close()
         config.add_section('octogit')
         config.set('octogit', 'username', '')
-        config.set('octogit', 'password', '')
+        config.set('octogit', 'token', '')
         return config
 
 
-def get_password():
+def get_token():
     config.read(CONFIG_FILE)
-    return config.get('octogit', 'password')
+    return config.get('octogit', 'token')
 
 
 def get_username():
@@ -40,12 +45,12 @@ def get_username():
     return config.get('octogit', 'username')
 
 
-def set_password(password):
+def set_token(token):
     '''
-    Given a config set the password attribute
+    Given a config set the token attribute
     in the Octogit section.
     '''
-    config.set('octogit', 'password', password)
+    config.set('octogit', 'token', token)
     commit_changes()
 
 
@@ -59,20 +64,25 @@ def set_username(username):
 
 
 def login(username, password):
-    r = requests.get('https://api.github.com', auth=(username, password))
-    if r.status_code == 200:
+    body = json.dumps({ "note": "octogit",
+                        "note_url": "https://github.com/myusuf3/octogit"})
+    r = requests.post('https://api.github.com/authorizations',
+            auth=(username, password), data=body)
+    if r.status_code == 201:
         puts(colored.green('You have successfully been authenticated with Github'))
     else:
         puts('{0}. {1}'.format(colored.blue('octogit'),
             colored.red('Do you even have a Github account? Bad Credentials')))
         sys.exit(3)
+    data = json.loads(r.content)
+    token = data["token"]
 
     if get_username() == username:
         pass
     else:
         set_username(username)
 
-    if get_password() == password:
+    if get_token() == token:
         pass
     else:
-        set_password(password)
+        set_token(token)
